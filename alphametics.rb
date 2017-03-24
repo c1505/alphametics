2f8745d4e4687862d688f66038414b62595b9028
c1505 <coreym247@gmail.com>
2017-03-24 17:23:32 +0000

    refactoring.  changed naming and added instance variables

--- alphametics.rb
+++ alphametics.rb
@@ -1,35 +1,36 @@
 class Alphametics
-  def self.solve(str)
-    self.new(str).run
-  end
   attr_accessor :result
-  def initialize(str)
-    @equation_string = str
-    @result = {}
-    @digits = (0..9).to_a
+  
+  def initialize(equation_string)
+    @equation_string = equation_string
+    @letters = @equation_string.scan(/[A-Z]/).uniq
+    @permutations = (0..9).to_a.permutation(@letters.length)
   end
+  
+  def self.solve(equation_string)
+    new(equation_string).run
+  end
+
   def run
-    letters = @equation_string.scan(/[A-Z]/).uniq
-    possible_answers = @digits.permutation(letters.length).to_a
-    possible_answers.each do |answer|
-      guess = Hash[letters.zip(answer)]
-      if check_math(guess)
-        return guess
-      end
+    @permutations.each do |permutation|
+      translator = Hash[@letters.zip(permutation)]
+      return translator if check_math(translator)
     end
-    return {}
+    {}
   end
-  def check_math(current_guess)
-    translated_equation = @equation_string.gsub(/[A-Z]/, current_guess)
-    return false unless valid_numbers?(translated_equation)
-    sides = translated_equation.split("==")
+  
+  def check_math(translator)
+    @translated_equation = @equation_string.gsub(/[A-Z]/, translator)
+    return false unless valid_numbers?
+    sides = @translated_equation.split("==")
     eval(sides[0]) == sides[1].to_i
   end
   
-  def valid_numbers?(translated_equation)
-    translated_equation.scan(/\d+/).each do |number|
+  private
+  
+  def valid_numbers?
+    @translated_equation.scan(/\d+/).each do |number|
       return false if number.to_i.to_s != number
     end
   end
-
 end
